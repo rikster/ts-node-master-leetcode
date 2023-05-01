4f5cdda6fdda232988ab54cc4aeaad55e905623c
Richard Hounslow <rhounslow@gmail.com>
2023-05-01 10:12:26 +1000

    Rearrange directory structure

--- src/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// do not remove -this file is require for the ts-node-dev commandline to work
-//"start": "ts-node-dev --respawn --transpile-only --watch src/*.ts --ignore-watch src/*.test.ts src/index.ts",
-//or use this as a start script
